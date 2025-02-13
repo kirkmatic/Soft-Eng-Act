b733d1f2e80c1e0bb5f7de3820a71c744cc76c49
Kirk J-Son Matic <kirkmatic1@gmail.com>
2025-02-12 20:11:29 -0800

    Add files via upload

---
+++ Alumni_System/includes/nav.php
@@ -0,0 +1,12 @@
+<?php
+    include ("header.php");
+?>
+    <nav class="bg-indigo-900 p-4 mb-4">
+        <ul class="flex space-between justify-between items-center px-6">
+            <li class="flex items-center">
+                <img src="/Alumni_System/assets/logos.png" alt="University Logo" class="w-12 h-auto">
+                <h1 class="text-white text-xl font-bold ml-2">Welcome</h1>
+            </li>
+            <li><a href="/Alumni_System/backend/logout_handler.php" class="text-white hover:text-gray-400 text-2xl"><i class="fas fa-sign-out-alt"></i></a></li>
+        </ul>
+    </nav>
